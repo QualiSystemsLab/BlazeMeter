c2136be09b74694683bde83e73a1e38f94b2768b
Yaniv Kalsky <yanivka@gmail.com>
2017-12-12 22:19:22 -0800

    Created a new service package
    Moved the relevant scripts into the driver
    Removed any old and irrelevant files.

--- Leads App - Service Virtualization/Resource Scripts/PrintMatrices.py
+++
@@ -1,5 +0,0 @@
-import os
-import json
-print("Quali Connectivity:\n{}".format(json.dumps(json.loads(os.environ["QUALICONNECTIVITYCONTEXT"]), indent=4)))
-print("Resservation:\n{}".format(json.dumps(json.loads(os.environ["RESERVATIONCONTEXT"]), indent=4)))
-print("Resource:\n{}".format(json.dumps(json.loads(os.environ["RESOURCECONTEXT"]), indent=4)))
